601ec86349adc6d4cd6f54efa1442d3aa454df4c
Kyle Ward <kyle@caliper.com>
2020-12-17 13:16:03 -0500

    Create the eda() function
    This function will run the same battery of tests and summaries on any scheme
    defined. The scheme is defined by four fields that will be used to create
    subgroups.

---
+++ docs/R/eda.R
@@ -0,0 +1,77 @@
+#' This functions performs a large number of tests and summaries
+#' of the survey data.
+#' 
+#' @param df The data.frame to process.
+#' @param tour_type Grouping column for tour type.
+#' @param homebased Grouping column for HB/NHB.
+#' @param purpose Grouping column for activity purpose.
+#' @param duration Grouping column for activity duration (e.g. "long"/"short")
+#' 
+#' @return A data frame with summaries and model coefficients for each market
+#'   segment. Market segments defined by the four grouping column names.
+
+eda <- function(df, tour_type = "tour_type", homebased = "homebased",
+                purpose = "purp_scheme1", duration = "dur_scheme1") {
+  
+  # Avoid the complication of programming with dplyr by creating new columns
+  df$tour_type <- df[[tour_type]]
+  df$homebased <- df[[homebased]]
+  df$purpose <- df[[purpose]]
+  df$duration <- df[[duration]]
+  
+  # Perform the EDA
+  eda <- df %>%
+    group_by(tour_type, homebased, purpose, duration) %>%
+    nest() %>%
+    mutate(
+      wTrips = map_dbl(
+        data, function(df) round(sum(df$trip_weight_combined, na.rm = TRUE), 0)),
+      r_size = map_dbl(data, function(df) {
+        model <- lm(trip_weight_combined ~ hhsize, data = df)
+        round(summary(model)$r.squared, 3)
+      }),
+      r_senior = map_dbl(data, function(df) {
+        model <- lm(trip_weight_combined ~ senior_present, data = df)
+        round(summary(model)$r.squared, 3)
+      }),
+      r_kid = map_dbl(data, function(df) {
+        model <- lm(trip_weight_combined ~ child_present, data = df)
+        round(summary(model)$r.squared, 3)
+      }),
+      r_income = map_dbl(data, function(df) {
+        model <- lm(trip_weight_combined ~ hh_income_midpt, data = df)
+        round(summary(model)$r.squared, 3)
+      }),
+      r_veh = map_dbl(data, function(df) {
+        model <- lm(trip_weight_combined ~ num_vehicles, data = df)
+        round(summary(model)$r.squared, 3)
+      }),
+      tod_summary = map(data, function(df) {
+        df %>%
+          group_by(tod) %>%
+          summarize(total = sum(trip_weight_combined, na.rm = TRUE)) %>%
+          mutate(
+            pct = round(total / sum(total) * 100, 1),
+            tod = paste0("pct_", tod)
+          ) %>%
+          select(-total) %>%
+          pivot_wider(names_from = "tod", values_from = "pct")
+      }),
+      mode_summary = map(data, function(df) {
+        df %>%
+          group_by(mode_simple2) %>%
+          summarize(total = sum(trip_weight_combined, na.rm = TRUE)) %>%
+          mutate(
+            pct = round(total / sum(total) * 100, 1),
+            mode_simple2 = paste0("pct_", mode_simple2)
+          ) %>%
+          select(-total) %>%
+          pivot_wider(names_from = "mode_simple2", values_from = "pct")
+      })
+    ) %>%
+    unnest(cols = c(tod_summary, mode_summary)) %>%
+    mutate(across(pct_bike:pct_walk, ~ifelse(is.na(.x), 0, .x))) %>%
+    select(-data) %>%
+    filter(tour_type != "H") %>%
+    arrange(tour_type, homebased, purpose, duration)
+}
