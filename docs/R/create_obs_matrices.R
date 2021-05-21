a154b3bd96b2f850c511197cff7a473022a58ec3
Kyle Ward <kyle@caliper.com>
2021-05-21 16:05:01 -0400

    Correct create_obs_matrices.R

--- docs/R/create_obs_matrices.R
+++ docs/R/create_obs_matrices.R
@@ -5,21 +5,21 @@ create_obs_matrices <- function(df) {
     tod <- df$board_time[i]
     access <- df$access_mode[i]
     mode <- df$mode[i]
-    sub_df <- df$data[[1]] %>%
+    sub_df <- df$data[[i]] %>%
       select(orig_taz, dest_taz, weight)
     
     view <- df_to_view(sub_df)
     file_name <- paste0("obs_", tod, "_", access, "_", mode, ".mtx")
+    file_name <- paste0(normalizePath("data/output/obs_processing"), "\\", file_name)
     mtx <- dk$CreateMatrixFromView(
       paste(tod, access, mode),
       paste0(view, "|"),
       "orig_taz",
       "dest_taz",
       list("weight"),
-      list(
-        "File Name" = paste0(normalizePath("data/output/obs_processing"), "\\", file_name)
-      )
+      list("File Name" = file_name)
     )
+    dk$CloseView(view)
   }
   
   disconnect()
