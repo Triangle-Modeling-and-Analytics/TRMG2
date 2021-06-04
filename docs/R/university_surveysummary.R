717866d8d45d7f4cffe4cfbc1124cb26f9b9ef4e
vamerlynck <81751783+vamerlynck@users.noreply.github.com>
2021-06-04 11:41:26 -0400

    TLD and mode choice survey

--- docs/R/university_surveysummary.R
+++ docs/R/university_surveysummary.R
@@ -26,38 +26,6 @@ trip_subset_filename <- paste0(private_dir,"Trip_subset_df.RDS")
 # Read data -------------------------------------------------------------------
 trip_subset_df <-readRDS(trip_subset_filename)
 
-# Histogram trip distance 
-
-Test_UC1_Tripdistance_histogram<-trip_subset_df %>% 
-  filter(Trip_Purpose == "UC1" & distance_miles<3)%>%
-  ggplot(aes(distance_miles,after_stat(density), color = On_campus, fill = On_campus)) + 
-  geom_histogram(position = "identity", alpha = 0.5)
-
-Test_UC1_Tripdistance_histogram + labs(title = "Trip Length Distribution - UC1", 
-                           subtitle = "for off-campus students (On_campus== 0) and on-campus students (On_campus == 1) students",  
-                           caption = "Source: NCSU survey (unweighted)")
-
-UC1_Oncampus_Tripdistance_histogram<-trip_subset_df %>% 
-  ungroup()%>%
-  filter(Trip_Purpose == "UC1" & On_campus == 1 & distance_miles<2)%>%
-  ggplot(aes(distance_miles,after_stat(density))) + 
-  geom_histogram()
-
-
-UC1_Oncampus_Tripdistance_histogram + labs(title = "Trip Length Distribution - UC1 Trips by On-campus students", 
-                              subtitle ="", 
-                              caption = ("Source: NCSU survey (unweighted)"))
-
-
-UC1_Offcampus_Tripdistance_histogram<-trip_subset_df %>% 
-  filter(Trip_Purpose == "UC1" & On_campus == 0 & distance_miles<2)%>%
-  ggplot(aes(distance_miles)) + 
-  geom_histogram()
-
-
-UC1_Offcampus_Tripdistance_histogram + labs(title = "Trip Length Distribution - UC1 Trips by Off-campus students", 
-                                           subtitle="", 
-                                           caption = ("Source: NCSU survey (unweighted)"))
 
 
 # Histogram trip distance zone to zone
@@ -71,7 +39,9 @@ zonetozonedistance_allpurposes_histogram<-trip_subset_df %>%
   geom_histogram(binwidth = 1) +
   facet_grid(On_campus ~ Trip_Purpose)
 
-zonetozonedistance_allpurposes_histogram
+zonetozonedistance_allpurposes_histogram + labs(title = "Trip Length Distribution (All purposes,except UC1)", 
+                                              subtitle="for off-campus students (On_campus== 0) and on-campus students (On_campus == 1) students", 
+                                              caption = ("Source: NCSU survey (unweighted)"))
 
 zonetozonedistance_UC1_histogram<-trip_subset_df %>%
   filter(Trip_Purpose =="UC1")%>%
@@ -79,43 +49,13 @@ zonetozonedistance_UC1_histogram<-trip_subset_df %>%
   ggplot(aes(distance_zonetozone)) +
   geom_histogram(binwidth = 1) +
   facet_grid(On_campus ~ .)
-zonetozonedistance_UC1_histogram
+zonetozonedistance_UC1_histogram + labs(title = "Trip Length Distribution (UC1 Trips)", 
+                                        subtitle="for off-campus students (On_campus== 0) and on-campus students (On_campus == 1) students", 
+                                        caption = ("Source: NCSU survey (unweighted)"))
 
-# Mode 
-# also split by Have_car
+# Mode Split
 
-
-trips_bypurpose_oncampus<-trip_subset_df %>%
-  filter(Trip_Purpose !="99") %>%
-  filter(!is.na(Walk)) %>%
-  filter(On_campus == 1) %>%
-  group_by(Trip_Purpose)%>%
-  summarize(Total = n(),
-            Bicycle = sum(Bicycle),
-            Walk = sum(Walk),
-            Car = sum(Car),
-            Carpool = sum(Carpool),
-            Bus = sum(Bus),
-            Other = sum(Other)) %>%
-  kable(digits = 1) %>%
-  kable_styling(full_width = FALSE)
-
-trips_bypurpose_offcampus<-trip_subset_df %>%
-  filter(Trip_Purpose !="99") %>%
-  filter(!is.na(Walk)) %>%
-  filter(On_campus == 0) %>%
-  group_by(Trip_Purpose)%>%
-  summarize(Total = n(),
-            Bicycle = sum(Bicycle),
-            Walk = sum(Walk),
-            Car = sum(Car),
-            Carpool = sum(Carpool),
-            Bus = sum(Bus),
-            Other = sum(Other)) %>%
-  kable(digits = 1) %>%
-  kable_styling(full_width = FALSE)
-
-trips_bypurpose_oncampus_pct<-trip_subset_df %>%
+trips_bypurpose_oncampus_pct_df<-trip_subset_df %>%
   filter(Trip_Purpose !="99") %>%
   filter(!is.na(Walk)) %>%
   filter(On_campus == 1) %>%
@@ -126,11 +66,13 @@ trips_bypurpose_oncampus_pct<-trip_subset_df %>%
             Car = sum(Car)/Total * 100,
             Carpool = sum(Carpool)/Total * 100,
             Bus = sum(Bus)/Total * 100,
-            Other = sum(Other)/Total * 100) %>%
-  kable(digits = 1) %>%
-  kable_styling(full_width = FALSE)
+            Other = sum(Other)/Total * 100)
+  #kable(digits = 1) %>%
+  #kable_styling(full_width = FALSE)
 
-trips_bypurpose_offcampus<-trip_subset_df %>%
+sumtripsoncampusbymode<-sum(trips_bypurpose_oncampus_pct$Total)
+
+trips_bypurpose_offcampus_pct_df<-trip_subset_df %>%
   filter(Trip_Purpose !="99") %>%
   filter(On_campus == 0) %>%
   filter(!is.na(Walk)) %>%
@@ -141,13 +83,73 @@ trips_bypurpose_offcampus<-trip_subset_df %>%
             Car = sum(Car)/Total * 100,
             Carpool = sum(Carpool)/Total * 100,
             Bus = sum(Bus)/Total * 100,
-            Other = sum(Other)/Total * 100) %>%
-  kable(digits = 1) %>%
-  kable_styling(full_width = FALSE)
+            Other = sum(Other)/Total * 100)
+sumtripsoffcampusbymode<-sum(trips_bypurpose_offcampus_pct$Total)
+
+# plots mode split
+
+trips_bypurpose_oncampus_plot <-trip_subset_df %>%
+  filter(Trip_Purpose != "99")%>%
+  filter(Primary_Mode != "NA")%>%
+  filter(On_campus == 1) %>%
+  filter(Trip_Purpose != "UC1") %>%
+  ggplot(aes(Trip_Purpose, fill=Primary_Mode)) + geom_bar(position="dodge")
+
+trips_bypurpose_oncampus_plot + labs(title = "Mode Split by Purpose (except UC1)", 
+                                     subtitle="On-campus students", 
+                                     caption = ("Source: NCSU survey (unweighted)"))
+
+trips_bypurpose_offcampus_plot <-trip_subset_df %>%
+  filter(Trip_Purpose != "99")%>%
+  filter(Primary_Mode != "NA")%>%
+  filter(Trip_Purpose != "UC1") %>%
+  filter(On_campus == 0) %>%
+  ggplot(aes(Trip_Purpose, fill=Primary_Mode)) + geom_bar(position="dodge")
+
+trips_bypurpose_offcampus_plot + labs(title = "Mode Split by Purpose (except UC1)", 
+                                     subtitle="Off-campus students", 
+                                     caption = ("Source: NCSU survey (unweighted)"))
+
+
+# mode split based on car availability 
+trips_bypurpose_oncampus_caravail_pct2_df<-trip_subset_df %>%
+  filter(Trip_Purpose !="99") %>%
+  filter(!is.na(Walk)) %>%
+  filter(On_campus == 1) %>%
+  filter(Have_car == "Yes" | (Have_car=="No" & Car!= 1)) %>%
+  group_by(Trip_Purpose,
+           Have_car)%>%
+  summarize(Total = n(),
+            Bicycle = sum(Bicycle)/Total * 100,
+            Walk = sum(Walk)/Total * 100,
+            Car = sum(Car)/Total * 100,
+            Carpool = sum(Carpool)/Total * 100,
+            Bus = sum(Bus)/Total * 100,
+            Other = sum(Other)/Total * 100) 
+
+
+  sumtripsoncampusbymode_clean<-sum(trips_bypurpose_oncampus_caravail_pct2$Total)
+
+trips_bypurpose_offcampus_caravail_pct2_df<-trip_subset_df %>%
+  filter(Trip_Purpose !="99") %>%
+  filter(On_campus == 0) %>%
+  filter(!is.na(Walk)) %>%
+  filter(Have_car == "Yes" | (Have_car=="No" & Car!= 1)) %>%
+  group_by(Trip_Purpose,
+           Have_car)%>%
+  summarize(Total = n(),
+            Bicycle = sum(Bicycle)/Total * 100,
+            Walk = sum(Walk)/Total * 100,
+            Car = sum(Car)/Total * 100,
+            Carpool = sum(Carpool)/Total * 100,
+            Bus = sum(Bus)/Total * 100,
+            Other = sum(Other)/Total * 100) 
+
+sumtripsoffcampusbymode_clean<-sum(trips_bypurpose_offcampus_caravail_pct2$Total)
 
 
 
-  
+
   
 
 
